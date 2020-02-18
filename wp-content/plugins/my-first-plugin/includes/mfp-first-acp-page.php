08f66a4a2c3eecc99198342a76cfead0e1c50738
Vinay Paudel <paudelvinay@gmail.com>
2020-02-18 12:06:52 +0545

    created my first plugin

---
+++ wp-content/plugins/my-first-plugin/includes/mfp-first-acp-page.php
@@ -0,0 +1,4 @@
+<div class="wrap">
+    <h1>Hello!</h1>
+    <p>This is my plugin's first page</p>
+</div>
--- wp-content/plugins/my-first-plugin/includes/mfp-functions.php
+++ wp-content/plugins/my-first-plugin/includes/mfp-functions.php
@@ -3,9 +3,18 @@
  * Add my new menu to the Admin Control Panel
  */
 
+
+// Hook the 'admin_menu' action hook, run the function named 'mfp_Add_My_Admin_Link()'
+add_action( 'admin_menu', 'mfp_Add_My_Admin_Link' );
+
 // Add a new top level menu link to the ACP
 function mfp_Add_My_Admin_link()
 {
+    add_menu_page(
+        'My First Page',
+        'My First Plugin',
+        'manage_options',
+        'my-first-plugin/includes/mfp-first-acp-page.php'
 
-//    My codes generates here
+    );
 }
