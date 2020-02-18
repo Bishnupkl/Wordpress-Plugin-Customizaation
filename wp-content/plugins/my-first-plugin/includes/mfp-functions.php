7d6bf2f2195999ce80c84ece6980c0b29c98b3e1
Vinay Paudel <paudelvinay@gmail.com>
2020-02-18 11:50:36 +0545

    adding own plugin folder and file started

---
+++ wp-content/plugins/my-first-plugin/includes/mfp-functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Add my new menu to the Admin Control Panel
+ */
+
+// Add a new top level menu link to the ACP
+function mfp_Add_My_Admin_link()
+{
+
+//    My codes generates here
+}
---
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ Plugin Name: My First Plugin
+ Description: This is my first plugin! It makes a new admin menu link!
+ Author: 20nupokhrel
+**/
+
+// Include mfp-functions.php, use require_once to stop the script if mfp-functions.php is not found
+require_once plugin_dir_path(__FILE__) . 'includes/mfp-functions.php';
+
+?>
