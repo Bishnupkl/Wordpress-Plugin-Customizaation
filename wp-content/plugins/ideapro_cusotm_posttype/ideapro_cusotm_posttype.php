2cf62e8f8f395cc1c1f051b2f6d3dc4b074b1de9
Vinay Paudel <paudelvinay@gmail.com>
2020-02-19 17:43:02 +0545

    showing example post in front page

--- wp-content/plugins/ideapro_cusotm_posttype/ideapro_cusotm_posttype.php
+++ wp-content/plugins/ideapro_cusotm_posttype/ideapro_cusotm_posttype.php
@@ -20,4 +20,18 @@ function ideapro_custom_posttype()
 }
 
 add_action('init','ideapro_custom_posttype');
+
+function get_example_post_types()
+{
+    $args = array('posts_per_page' => -1,
+        'post_type'=>'example');
+    $ourPosts = get_posts($args);
+
+    foreach ($ourPosts as $key=>$values) {
+        print '<a href="'.get_permalink($values->ID).'"><strong>'. $values->post_title.'</strong></<a href=""></a> <br />';
+        print $values->post_content.' <br />';
+    }
+}
+
+add_shortcode('get_example_posts','get_example_post_types');
 ?>
